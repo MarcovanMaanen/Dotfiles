ec9a08bb703c6f0407daeb85f935cc19f236b12b
Marco van Maanen <phpjunky87@gmail.com>
2023-12-02 22:48:04 +0100

    Update Microsoft.PowerShell_profile.ps1

--- Windows/Microsoft.PowerShell_profile.ps1
+++ Windows/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,20 @@
+New-Alias g goto
+
+function goto {
+    param (
+        $location
+    )
+
+    Switch ($location) {
+        "pr" {
+            Set-Location -Path "$HOME/projects"
+        }
+        default {
+            echo "Invalid location"
+        }
+    }
+}
+
 $ENV:STARSHIP_CONFIG = "$HOME\.starship\starship.toml"
 $ENV:STARSHIP_CACHE = "$HOME\AppData\Local\Temp"
 Invoke-Expression (&starship init powershell)
